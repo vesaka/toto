6295c93d7db51dc47cdeb813ed0fd2b8fdf8680f
Veselin <vesakabgr@gmail.com>
2023-02-25 03:25:17 +0200

    use env.js as plugin

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ import './style.css';
 import { createApp } from 'vue';
 import { createRouter, createWebHashHistory, createWebHistory  } from 'vue-router';
 
+
 import App from './App.vue';
+import env from './bootstrap/env';
 import pinia from './bootstrap/pinia';
 import router from './bootstrap/router';
 createApp(App)
+    .use(env)
     .use(router)
     .use(pinia)
     .mount('#app');
