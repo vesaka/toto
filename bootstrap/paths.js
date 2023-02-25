eb1bc07fbf5b2c034b72c8bbabb0fc44aabd7228
Veselin <vesakabgr@gmail.com>
2023-02-25 03:49:08 +0200

    take base url from settings.json

--- bootstrap/paths.js
+++ bootstrap/paths.js
@@ -1,4 +1,4 @@
-export const BASE = '/';
+export const BASE = env.base || '/';
 export const PLAY = BASE + 'playground';
 export const PAGE_404 = BASE + '404';
 export const LEADERBOARD = BASE + 'leaderboard';
