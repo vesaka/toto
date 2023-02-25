6aab1a5824ac0b5ae4aa0d8f5b764b36d61bd8d8
Veselin <vesakabgr@gmail.com>
2023-02-25 03:55:53 +0200

    take base url from settings.json

--- bootstrap/env.js
+++ bootstrap/env.js
@@ -1,8 +1,9 @@
 import settings from '$toto/config/settings.json';
+window.env = settings;
 export default {
     install(app) {
         app.config.globalProperties.$env = settings;
-        window.env = settings;
+        
     }
 };
 
